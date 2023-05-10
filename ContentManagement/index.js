d9e9fa36d296dda65d2f18e08079ede4d638a944
cyrine lounissi <cyrinelounissi@MacBook-Air-de-Cyrine.local>
2023-05-10 23:12:30 +0100

    Demo commit

--- ContentManagement/index.js
+++ ContentManagement/index.js
@@ -309,6 +309,6 @@ app.use('/content/video/:videoName(*)', (req, res) => {
   });
 });
 app.listen(port, () => {
-  console.log('Starting         nchallah');
+  console.log('Starting   !!');
   console.log(`Server listening on port   4000`);
 });
